474a445b2642329db609858d5d0f229d1e5f3ca2
Jakub Adamski <kuba@exemplum.pl>
2021-01-13 20:52:55 +0100

    admin-fix

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -2,18 +2,19 @@
     <div>
         <div>Admin page</div>
         <input id="password" placeholder="Enter password" type="password" v-model="password">
-        <div class="get-button" v-on:click="getPost()">Get post</div>
-        <select id="mode" v-model="mode">
+        <div class="get-button" :class="$mq" v-on:click="getPost()">Get post</div>
+        <select id="mode" v-model="mode" class="selector" placeholder="select mode">
             <option>New posts</option>
             <option>Reported</option>
+            <option>No humanreview</option>
         </select>
         <div class="sep"></div>
         <div>Posts to be reviewed in selected mode: {{ left }}</div>
-        <div>Spam probability {{ spam*100 }}%</div>
+        <div class="spam" :style="'color: ' + (spam > 0.5 ? 'red;' : 'green;' )">Spam probability {{ Math.round(spam*100) }}%</div>
         <div class="sep"></div>
         <div class="action" v-on:click="review(false)">Mark as good</div>
         <div class="action" v-on:click="review(true)">Block post</div>
-        <Post :post="post" v-if="post && left!=0"/>
+        <Post class="post" :class="$mq" :post="post" v-if="post && left!=0"/>
     </div>
 </template>
 
@@ -42,11 +43,11 @@ export default Vue.extend({
                 }
             }
             const query = gql`
-            mutation review($postID: String!, $new: Boolean!, $delete: Boolean!, $password: String!) {
+            mutation review($postID: String!, $type: Int!, $delete: Boolean!, $password: String!) {
                     review(
                         input: {
                         postID: $postID
-                        new: $new
+                        type: $type
                         delete: $delete
                         password: $password
                         }
@@ -54,12 +55,22 @@ export default Vue.extend({
                 }
             `
 
-            const variables = {
+            var variables = {
                 postID: this.post.ID.slice(10, -2),
-                new: this.mode=="New posts" ? true : false,
+                type: 0,
                 delete: deleteMode,
                 password: this.password
             }
+            if (this.mode == "New posts") {
+                variables.type = 0
+
+            } else if (this.mode == "Reported") {
+                variables.type = 1
+
+            } else {
+                variables.type = 2
+            }
+
             this.client.request(query, variables)
                 .then(response => {
                     if (!response.review){
@@ -72,8 +83,8 @@ export default Vue.extend({
         },
         getPost(){
             const query = gql`
-            query getForReview($password: String!, $new: Boolean!) {
-                    unReviewed(password: $password, new: $new) {
+            query getForReview($password: String!, $type: Int!) {
+                    unReviewed(password: $password, type: $type) {
                         post {
                             ID
                             text
@@ -90,10 +101,20 @@ export default Vue.extend({
                 }
             `
 
-            const variables = {
+            var variables = {
                 password: this.password,
-                new: this.mode=="New posts" ? true : false
+                type: 0
             }
+            if (this.mode == "New posts") {
+                variables.type = 0
+
+            } else if (this.mode == "Reported") {
+                variables.type = 1
+
+            } else {
+                variables.type = 2
+            }
+
             this.client.request(query, variables)
                 .then(response => {
                     this.post = response.unReviewed.post
@@ -108,8 +129,18 @@ export default Vue.extend({
 
 <style lang="sass" scoped>
 
+.spam
+    font-weight: bold
+    font-size: 17px
+
 .sep
     margin-top: 50px
+
+.post
+    &.md
+        margin-top: 150px
+    &.sm
+        margin-top: 250px
 
 
 .action
@@ -117,17 +148,23 @@ export default Vue.extend({
 
 
 .get-button
-    display: inline
+    display: inline-block
     cursor: pointer
     border: 1px solid white
     padding: 5px 10px
 
+.selector
+    background: var(--bg)
+    color: var(--color)
+    font-size: 16px
+    padding: 5px 10px
 
 #password
-    background: rgb(28, 29, 30)
+    background: var(--bg)
+    color: var(--color)
+    margin-top: 10px
     outline: none
-    border: none
-    color: white
+    border: 1px solid var(--color)
     font-size: 16px
     padding: 5px 10px
 
